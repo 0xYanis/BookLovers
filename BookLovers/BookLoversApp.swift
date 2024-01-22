9573bd454411be349a332cf8864831a1bb1bb57e
Yanis Rybkin <ynchprod@gmail.com>
2024-01-22 17:11:03 +0300

    Initial Commit

---
+++ BookLovers/BookLoversApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookLoversApp.swift
+//  BookLovers
+//
+//  Created by Yanis on 22.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookLoversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookLovers/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BookLovers
+//
+//  Created by Yanis on 22.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
