16b8a6eae64c84963f68a05553d6472ac56e3646
Yanis Rybkin <ynchprod@gmail.com>
2024-02-05 15:09:43 +0300

    Created onSwipe extension to View to detect swipes

---
+++ BookLovers/Extensions/View+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  View+Extension.swift
+//  BookLovers
+//
+//  Created by Yanis on 05.02.2024.
+//
+
+import SwiftUI
+
+extension View {
+    func onSwipe(
+        up: @escaping (() -> Void) = {},
+        down: @escaping (() -> Void) = {},
+        left: @escaping (() -> Void) = {},
+        right: @escaping (() -> Void) = {}
+    ) -> some View {
+        return self.gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+            .onEnded({ value in
+                if value.translation.width < 0 { left() }
+                if value.translation.width > 0 { right() }
+                if value.translation.height < 0 { up() }
+                if value.translation.height > 0 { down() }
+            }))
+    }
+}
