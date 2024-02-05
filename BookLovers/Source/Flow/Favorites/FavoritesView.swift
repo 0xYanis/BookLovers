4def18d9b19c2001fd2949495269a34e8c2d3b7e
Yanis Rybkin <ynchprod@gmail.com>
2024-02-05 12:44:07 +0300

    Created FavoritesView

---
+++ BookLovers/Source/Flow/Favorites/FavoritesView.swift
@@ -0,0 +1,22 @@
+//
+//  FavoritesView.swift
+//  BookLovers
+//
+//  Created by Yanis on 05.02.2024.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        NavigationStack {
+            
+        }
+    }
+}
+
+struct FavoritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesView()
+    }
+}
