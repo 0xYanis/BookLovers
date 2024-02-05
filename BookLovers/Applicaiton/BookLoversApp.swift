3e21875cfa19cf0118ea5b1e9ce5fad706fa4a01
Yanis Rybkin <ynchprod@gmail.com>
2024-02-05 13:50:15 +0300

    Removed all old objects & created Tabbar

--- BookLovers/Applicaiton/BookLoversApp.swift
+++ BookLovers/Applicaiton/BookLoversApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BookLoversApp: App {
     var body: some Scene {
         WindowGroup {
-            FlowView()
+            MainTabbarView()
         }
     }
 }
---
+++ BookLovers/Core/Tabbar/MainTabbarView.swift
@@ -0,0 +1,71 @@
+//
+//  MainTabbarView.swift
+//  BookLovers
+//
+//  Created by Yanis on 05.02.2024.
+//
+
+import SwiftUI
+
+fileprivate enum TabItem: String, Identifiable, CaseIterable {
+    case explore
+    case favorites
+    case community
+    
+    var id: String { rawValue }
+    var name: String { rawValue }
+    
+    var image: String {
+        switch self {
+        case .explore  : return "magnifyingglass"
+        case .favorites: return "heart"
+        case .community: return "person.3"
+        }
+    }
+    
+    var filledImage: String {
+        guard self != .explore else { return image }
+        return "\(image).fill"
+    }
+    
+    @ViewBuilder
+    var view: some View {
+        switch self {
+        case .explore  : Text("Explore")
+        case .favorites: Text("Favorites")
+        case .community: Text("Community")
+        }
+    }
+    
+    func uiImage(selected: TabItem) -> UIImage {
+        UIImage(
+            systemName: selected == self ?
+            filledImage : image
+        ) ?? UIImage()
+    }
+}
+
+struct MainTabbarView: View {
+    @State private var visibility: Visibility = .automatic
+    @State private var selectedTab: TabItem = .explore
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            ForEach(TabItem.allCases) { item in
+                item.view
+                    .tabItem {
+                        Image(uiImage: item.uiImage(selected: selectedTab))
+                        Text(item.name)
+                    }
+                    .tag(item)
+                    .toolbar(visibility, for: .tabBar)
+            }
+        }
+    }
+}
+
+struct MainTabbarView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabbarView()
+    }
+}
