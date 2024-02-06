845933daed23b1951718891fdf229ddd3fe25dd5
Yanis Rybkin <ynchprod@gmail.com>
2024-02-06 14:56:55 +0300

    Replaced tab item

--- BookLovers/Core/Tabbar/MainTabbarView.swift
+++ BookLovers/Core/Tabbar/MainTabbarView.swift
@@ -7,51 +7,6 @@
 
 import SwiftUI
 
-fileprivate enum TabItem: String, Identifiable, CaseIterable {
-    case explore
-    case favorites
-    case community
-    
-    var id: String { rawValue }
-    var name: String { rawValue }
-    var image: String {
-        switch self {
-        case .explore  : return "magnifyingglass"
-        case .favorites: return "heart"
-        case .community: return "person.3"
-        }
-    }
-    
-    var filledImage: String {
-        guard self != .explore else { return image }
-        return "\(image).fill"
-    }
-    
-    var badgeNumber: Int {
-        switch self {
-        case .explore  : return 0
-        case .favorites: return 0
-        case .community: return 4
-        }
-    }
-    
-    @ViewBuilder
-    var view: some View {
-        switch self {
-        case .explore  : ExploreView()
-        case .favorites: FavoritesView()
-        case .community: Text("Community")
-        }
-    }
-    
-    func uiImage(selected: TabItem) -> UIImage {
-        UIImage(
-            systemName: selected == self ?
-            filledImage : image
-        ) ?? UIImage()
-    }
-}
-
 struct MainTabbarView: View {
     @State private var visibility: Visibility = .automatic
     @State private var selectedTab: TabItem = .explore
---
+++ BookLovers/Core/Tabbar/TabItem.swift
@@ -0,0 +1,54 @@
+//
+//  TabItem.swift
+//  BookLovers
+//
+//  Created by Yanis on 06.02.2024.
+//
+
+import SwiftUI
+
+enum TabItem: String, Identifiable, CaseIterable {
+    case explore
+    case favorites
+    case community
+    
+    var id: String { rawValue }
+    var name: String { rawValue }
+    
+    var image: String {
+        switch self {
+        case .explore  : return "magnifyingglass"
+        case .favorites: return "heart"
+        case .community: return "person.3"
+        }
+    }
+    
+    var filledImage: String {
+        guard self != .explore else { return image }
+        return "\(image).fill"
+    }
+    
+    var badgeNumber: Int {
+        switch self {
+        case .explore  : return 0
+        case .favorites: return 0
+        case .community: return 4
+        }
+    }
+    
+    @ViewBuilder
+    var view: some View {
+        switch self {
+        case .explore  : ExploreView()
+        case .favorites: FavoritesView()
+        case .community: Text("Community")
+        }
+    }
+    
+    func uiImage(selected: TabItem) -> UIImage {
+        UIImage(
+            systemName: selected == self ?
+            filledImage : image
+        ) ?? UIImage()
+    }
+}
