9abb65b18be2120e82baa1f76583bc90cf340229
Yanis Rybkin <ynchprod@gmail.com>
2024-02-13 17:42:36 +0300

    Created AuthView

--- BookLovers/Components/Items/XButton.swift
+++ BookLovers/Components/Items/XButton.swift
@@ -13,7 +13,7 @@ struct XButton: View {
     var body: some View {
         Button(action: action) {
             Image(systemName: "xmark.circle.fill")
-                .font(.title2)
+                .font(.title)
                 .foregroundStyle(.secondary)
         }
         .buttonStyle(.plain)
