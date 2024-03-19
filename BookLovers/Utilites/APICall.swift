b783673840aa15a432ff5aded6907413eb4f252c
Yanis Rybkin <ynchprod@gmail.com>
2024-03-19 16:09:12 +0300

    Created network interfaces

---
+++ BookLovers/Utilites/APICall.swift
@@ -0,0 +1,55 @@
+//
+//  APICall.swift
+//  BookLovers
+//
+//  Created by Yanis on 19.03.2024.
+//
+
+import Foundation
+
+protocol APICall {
+    var path: String { get }
+    var method: String { get }
+    var headers: [String: String]? { get }
+    
+    func body() throws -> Data?
+}
+
+enum APIError: Error {
+    case invalidURL
+    case httpCode(_ code: HTTPCode)
+    case unexpectedResponse
+}
+
+extension APIError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "Invalid URL"
+        case .httpCode(let code):
+            return "Unexpected HTTP code: \(code)"
+        case .unexpectedResponse:
+            return "Unexpected response from server"
+        }
+    }
+}
+
+extension APICall {
+    func urlRequest(baseURL: String) throws -> URLRequest {
+        guard let url = URL(string: baseURL + path) else {
+            throw APIError.invalidURL
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = method
+        request.allHTTPHeaderFields = headers
+        request.httpBody = try body()
+        return request
+    }
+}
+
+typealias HTTPCode = Int
+typealias HTTPCodes = Range<HTTPCode>
+
+extension HTTPCodes {
+    static let success = 200 ..< 300
+}
